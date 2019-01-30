620ab73154e3b7b296570475a278cb1341222ee4
bigfabbro93 <fabrizio.dascenzo@student.univaq.it>
2019-01-30 17:06:19 +0100

    implementati alcuni metodi

--- Domain/Turn.java
+++ Domain/Turn.java
@@ -2,14 +2,12 @@ public class Turn {
 
 	CombatPhase CPhase;
 
-	public boolean newCombatPhase() {
-		// TODO - implement Turn.newCombatPhase
-		throw new UnsupportedOperationException();
+	public void newCombatPhase() {
+		this.CPhase = new CombatPhase();
 	}
 
 	public CombatPhase getCombatPhase() {
-		// TODO - implement Turn.getCombatPhase
-		throw new UnsupportedOperationException();
+		return this.CPhase;
 	}
 
 }
